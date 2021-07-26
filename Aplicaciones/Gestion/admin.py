960816b50c06b63fa76cd92c574c45b8c74ad869
juampipescie <76536672+juanpescie@users.noreply.github.com>
2021-07-26 16:02:52 -0300

    add(boiler plate) add(Aplicaciones/Gestion/models.py) add the models of Persona, Email, CIudad, Telefono

---
+++ Aplicaciones/Gestion/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from Aplicaciones.Gestion.models import *
+# Register your models here.
+admin.site.register(Ciudad)
+admin.site.register(Telefono)
+admin.site.register(Persona)
+admin.site.register(Email)
---
+++ Aplicaciones/Gestion/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+# MODELO CIUDAD
+class Ciudad(models.Model):
+    codigo = models.PositiveSmallIntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+
+generos = [("F", "Femenino"), ("M", "Masculino"), ("X", "Otro")]
+# MODELO PERSONA
+class Persona(models.Model):
+    dni = models.CharField(max_length=11, primary_key=True)
+    apellido = models.CharField(max_length=50)
+    nombres = models.CharField(max_length=50)
+    sexo = models.CharField(max_length=1, choices=generos, default="F")
+    fechaNacimiento = models.DateField()
+    ciudad = models.ForeignKey(Ciudad, null=False, blank=False, on_delete=models.CASCADE)
+    
+
+    def nombreCompleto(self):
+        return f"{self.apellido}, {self.nombres}"
+
+class Telefono(models.Model):
+    persona = models.ForeignKey(Persona, null=False, blank=False, on_delete=models.CASCADE)
+    numero = models.CharField(max_length=15)
+
+class Email(models.Model):
+    persona = models.ForeignKey(Persona, null=False, blank=False, on_delete=models.CASCADE)
+    email = models.CharField(max_length=100)
+    
+
+
