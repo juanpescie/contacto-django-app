de5bc7f1cfa6039f5f79366b784c4308f471ba1c
juampipescie <76536672+juanpescie@users.noreply.github.com>
2021-07-27 01:24:43 -0300

    .

--- Aplicaciones/Gestion/views.py
+++ Aplicaciones/Gestion/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
 from django.conf import settings
+import os 
+from dotenv import load_dotenv
+load_dotenv()
 
 # Create your views here.
 def bienvenida(request):
@@ -14,7 +17,7 @@ def contactar(request):
         asunto = request.POST["txtAsunto"]
         mensaje = request.POST["txtMensaje"] + " / Email: " + request.POST["txtEmail"]
         email_from = settings.EMAIL_HOST_USER
-        email_to = ["juanppescie@gmail.com"]
+        email_to = [os.getenv("EMAIL_HOST_USER")]
         send_mail(asunto, mensaje, email_from, email_to, fail_silently=True)
         return render(request, "contactoExitoso.html")
     return render(request, "formularioContacto.html")
